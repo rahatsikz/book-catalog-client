fb8b35d210261777ae613e19324fa0606e6a49e9
rahatsikz <rascal6271@gmail.com>
2023-07-13 22:59:09 +0600

    [DONE] ~ book list on home page added

---
+++ src/components/TopBooks.tsx
@@ -0,0 +1,30 @@
+export default function TopBooks() {
+  const bookNames = [
+    "To Kill a Mockingbird",
+    "1984",
+    "Pride and Prejudice",
+    "The Great Gatsby",
+    "The Lord of the Rings",
+    "Harry Potter and the Sorcerer's Stone",
+    "The Catcher in the Rye",
+    "Moby-Dick",
+    "The Chronicles of Narnia",
+    "The Da Vinci Code",
+  ];
+
+  return (
+    <div className="container mx-auto mt-12">
+      <p className="text-center text-xl font-semibold underline text-cyan-600 underline-offset-8 uppercase">
+        Here are the latest top books
+      </p>
+
+      <div className="grid lg:grid-cols-3 gap-6 mt-12">
+        {bookNames.map((book, idx) => (
+          <p key={idx} className="text-lg font-semibold">
+            {book}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import TopBooks from "../components/TopBooks";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <TopBooks />
     </div>
   );
 }
